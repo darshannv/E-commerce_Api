835bbac0dd12c18283927f6a0b7cfe0cad23bad1
darshannv <darshanvasu3048@gmail.com>
2023-06-16 22:46:07 +0530

    api-update

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,176 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use App\Models\Variant;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $products = Product::with('variants')->latest()->get();
+
+        return response()->json($products);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+        
+        $request->validate([
+            'name' => 'required',
+            'description' => 'required',
+            'price' => 'required|numeric',
+            'variants' => 'required|array',
+            '*.variants.*.name' => 'required',
+            '*.variants.*.sku' => 'required',
+            '*.variants.*.additional_cost' => 'required|numeric',
+            '*.variants.*.stock_count' => 'required|integer'
+
+        ]);
+
+        //dd($request->all());
+
+        $product = Product::create([
+            'name' => $request->name,
+            'description' => $request->description,
+            'price' => $request->price     
+        ]);
+
+
+        $variants = [];
+        foreach($request->input('variants') as $variant){
+            $variants[] = new Variant($variant); 
+        }
+
+        $product->variants()->saveMany($variants);
+
+        return response()->json($product, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Product $product)
+    {
+        $detail = Product::with('variants')->findOrFail($product->id);
+        //dd($detail);
+
+        return response()->json($detail);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Product $product)
+    {
+        $detail = Product::with('variants')->findOrFail($product->id);
+        return response()->json($detail);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Product $product)
+    {
+       
+        $request->validate([
+            'name' => 'required',
+            'description' => 'required',
+            'price' => 'required|numeric',
+            'variants' => 'required|array',
+            'variants.*.name' => 'required',
+            'variants.*.sku' => 'required',
+            'variants.*.additional_cost' => 'required|numeric',
+            'variants.*.stock_count' => 'required|integer'
+
+        ]);
+
+        $product = Product::findOrFail($product->id);
+        $product->update([
+            'name' => $request->name,
+            'description' => $request->description,
+            'price' => $request->price     
+        ]);
+
+        $variants = [];
+        foreach($request->input('variants') as $variant){
+            if(isset($variant['id'])){
+                $variant_data = Variant::findOrFail($variant['id']);
+                $variant_data->update($variant);
+                $variants[] = $variant;
+            } else {
+                $variants[] = new Variant($variant);
+            }
+             
+        }
+
+
+        $product->variants()->delete();
+        $product->variants()->saveMany($variants);
+
+
+        return response()->json($product);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Product $product)
+    {
+        $detail = Product::findOrFail($product);
+        $detail->delete();
+        return response()->json(null, 204);
+    }
+
+
+    public function search($query){
+
+        
+        //dd($query);
+        $products = Product::where('name', 'like', "%{$query}%")
+                ->orWhere('description', 'like', "%{$query}%")
+                ->orWhereHas('variants', function($q) use ($query) {
+                    $q->where('name', 'like', "%{$query}%");
+                })
+                ->with('variants')->get();
+
+        return response()->json($products);
+    }
+}
---
+++ database/migrations/2023_06_16_134433_create_variants_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('variants', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('product_id');
+            $table->string('name');
+            $table->string('sku');
+            $table->decimal('aditional_cost', 8, 2);
+            $table->integer('stock_count');
+            $table->timestamps();
+
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('variants');
+    }
+};
